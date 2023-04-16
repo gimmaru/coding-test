54b58c7b9b1e95fcdf16fd777e52a526743b538c
gimmaru <john8778@naver.com>
2023-04-16 21:48:11 +0900

    sol(merge-two-sorted-lists): solution with recursion

--- leetcode/merge-two-sorted-lists.py
+++ leetcode/merge-two-sorted-lists.py
@@ -8,34 +8,8 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not list2:
-            return list1
-        if not list1:
-            return list2
-        
-        result = list()
-        while list1 and list2:
-            if list1.val <= list2.val:
-                result.append(list1.val)
-                list1 = list1.next
-            else:
-                result.append(list2.val)
-                list2 = list2.next
-        
-        while list1:
-            result.append(list1.val)
-            list1 = list1.next
-        
-        while list2:
-            result.append(list2.val)
-            list2 = list2.next
-        
-        ll_result = None
-        for v in result:
-            if not ll_result:
-                ll_result = ListNode(v)
-            else:
-                ll_result = ListNode(v, ll_result)
-        
-        return ll_result
-            
+        if (not list1) or (list2 and list1.val > list2.val):
+            list2, list1 = list1, list2
+        if list1:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+        return list1

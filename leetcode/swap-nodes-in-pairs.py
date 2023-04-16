0d671bdc52630650613ed70bc5276daaf09af3fe
gimmaru <john8778@naver.com>
2023-04-17 01:12:50 +0900

    sol(swap-nodes-in-pairs): initial solution

---
+++ leetcode/swap-nodes-in-pairs.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head and head.next:
+            temp = ListNode(head.val, head.next.next)
+            head = head.next
+            head.next = temp
+            head.next.next = self.swapPairs(head.next.next)
+        return head

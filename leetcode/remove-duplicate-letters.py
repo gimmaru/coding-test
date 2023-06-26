e1d21a0ee4a3d44f5b533efc8dfbda6bd8c52fa9
gimmaru <john8778@naver.com>
2023-06-26 20:17:01 +0900

    add new solution using recursion

--- leetcode/remove-duplicate-letters.py
+++ leetcode/remove-duplicate-letters.py
@@ -22,3 +22,11 @@ class Solution:
             table[char] -= 1
                 
         return ''.join(stack)
+    
+    def removeDuplicateLetters_recursive(self, s: str) -> str:        
+        for char in sorted(set(s)):
+            suffix = s[s.index(char):]
+            
+            if set(s) == set(suffix):
+                return char + self.removeDuplicateLetters(suffix.replace(char, ''))
+        return ''

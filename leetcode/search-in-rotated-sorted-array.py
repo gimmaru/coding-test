d8276e138e9cbb460576403b14c4329bbfb6fcb4
gimmaru <john8778@naver.com>
2023-08-07 20:58:14 +0900

    add solution

---
+++ leetcode/search-in-rotated-sorted-array.py
@@ -0,0 +1,63 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] < nums[right]:
+                right = mid
+            elif nums[mid] > nums[left]:
+                left = mid
+            else:
+                break
+        
+        mid = left + (right - left) // 2
+        if nums[0] <= target <= nums[mid]:
+            left, right = 0, mid
+        else:
+            left, right = mid + 1, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+        
+        return -1
+
+'''
+* 책에 나온 풀이
+'''
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            else:
+                right = mid
+        
+        pivot = left
+
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            mid_pivot = (mid + pivot) % len(nums)
+
+            if nums[mid_pivot] < target:
+                left = mid + 1
+            elif nums[mid_pivot] > target:
+                right = mid - 1
+            else:
+                return mid_pivot
+        return -1

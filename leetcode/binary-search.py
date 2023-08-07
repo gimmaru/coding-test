1a7fbc0bef04c928748f43520e3e65c187b1d723
gimmaru <john8778@naver.com>
2023-08-07 16:55:35 +0900

    sol(binary-search): add the simplest solution

---
+++ leetcode/binary-search.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        try:
+            result = nums.index(target)
+        except ValueError:
+            result = -1
+        return result

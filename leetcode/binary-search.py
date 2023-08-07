cbaf2b0ff23b19ca208e129f88fc808150c5e410
gimmaru <john8778@naver.com>
2023-08-07 17:46:11 +0900

    sol(binary-search): add solutions

--- leetcode/binary-search.py
+++ leetcode/binary-search.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         try:
@@ -39,3 +40,49 @@ class Solution:
             return binary_search(left, right)
         
         return binary_search(0, len(nums) - 1)
+
+'''
+* 책에 나온 풀이
+'''
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def binary_search(left, right):
+            if left <= right:
+                mid = left + (right - left) // 2
+                
+                if target > nums[mid]:
+                    return binary_search(mid + 1, right)
+                elif target < nums[mid]:
+                    return binary_search(left, mid - 1)
+                else:
+                    return mid
+            else:
+                return -1
+        
+        return binary_search(0, len(nums) - 1)
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+        return -1
+
+
+import bisect
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        index = bisect.bisect_left(nums, target)
+
+        if index < len(nums) and nums[index] == target:
+            return index
+        else:
+            return -1

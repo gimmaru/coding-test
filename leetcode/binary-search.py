a61e8fde2906131b5000717823b982830dd37c21
gimmaru <john8778@naver.com>
2023-08-07 17:33:48 +0900

    sol(binary-search): add recursion solution

--- leetcode/binary-search.py
+++ leetcode/binary-search.py
@@ -21,3 +21,21 @@ class Solution:
             else:
                 return mid
         return -1
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def binary_search(left, right):
+            if left > right:
+                return -1
+            
+            mid = (left + right) // 2
+
+            if target > nums[mid]:
+                left = mid + 1
+            elif target < nums[mid]:
+                right = mid - 1
+            else:
+                return mid
+            return binary_search(left, right)
+        
+        return binary_search(0, len(nums) - 1)

74867b30bbdb1a3c0aad1f664ef60949e0cff593
gimmaru <john8778@naver.com>
2023-11-08 21:33:33 +0900

    add solution

---
+++ programmars/mysql/입양_시각_구하기_2.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SET @hour := -1; -- 변수 선언
+
+SELECT (@hour := @hour + 1) AS HOUR,
+       (SELECT COUNT(*) 
+           FROM ANIMAL_OUTS 
+           WHERE HOUR(DATETIME) = @hour
+       ) AS COUNT
+FROM ANIMAL_OUTS
+WHERE @hour < 23
+
+-- https://chanhuiseok.github.io/posts/db-6/

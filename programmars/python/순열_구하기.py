6fa375069666e391c2adf1148be94503e0fb82db
gimmaru <john8778@naver.com>
2024-01-06 18:09:29 +0900

    add solution

---
+++ programmars/python/순열_구하기.py
@@ -0,0 +1,22 @@
+def permute(N, M):
+    nums = list(range(1, N+1))
+    result = []
+
+    def dfs(nums, output=[]):
+        if len(output) == M:
+            result.append(output)
+            return
+
+        for num in nums:
+            output.append(num)
+            next_nums = nums[:]
+            next_nums.remove(num)
+            dfs(next_nums, output[:])
+            output.pop()
+        
+    dfs(nums)
+    return result
+
+if __name__ == "__main__":
+    print(permute(4, 2))
+
